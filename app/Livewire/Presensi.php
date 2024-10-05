8f93d13ff9266639982c8f612395245e22509673
mddirga <madedirga22@gmail.com>
2024-10-05 18:49:27 +0800

    Displays Office and Employee Current Location

--- app/Livewire/Presensi.php
+++ app/Livewire/Presensi.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Schedule;
 use Livewire\Component;
+use Auth;
 
 class Presensi extends Component
 {
     public function render()
     {
-        return view('livewire.presensi');
+        $schedule = Schedule::where('user_id', Auth::user()->id)->first();
+        return view('livewire.presensi', [
+            'schedule' => $schedule
+        ]);
     }
 }
--- resources/views/livewire/presensi.blade.php
+++ resources/views/livewire/presensi.blade.php
@@ -6,21 +6,62 @@
                     <h2 class="text-2xl font-bold mb-2">Informasi Karyawan</h2>
                     <div class="bg-gray-100 p-4">
                         <p><strong>Nama Karyawan : </strong>{{ Auth::user()->name }}</p>
-                        <p><strong>Kantor : </strong>Kantor Utama</p>
-                        <p><strong>Shift : </strong>Pagi</p>
+                        <p><strong>Kantor : </strong>{{ $schedule->office->name }}</p>
+                        <p><strong>Shift : </strong>{{ $schedule->shift->name }} ({{ $schedule->shift->start_time }} - {{ $schedule->shift->end_time }}) WITA</p>
                     </div>
                 </div>
                 <div>
                     <h2 class="text-2xl font-bold mb-2">Presensi</h2>
                     <div id="map" class="mb-4 rounded-lg border border-gray-300"></div>
-                    <button type="button" class="px-4 py-2 bg-blue-500 text-white rounded">Tag Location</button>
+                    <button type="button" onclick="tagLocation()" class="px-4 py-2 bg-blue-500 text-white rounded">Tag Location</button>
+                    <button type="button" class="px-4 py-2 bg-green-500 text-white rounded">Submit Presensi</button>
                 </div>
             </div>
         </div>
     </div>
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <script>
-        const map = L.map('map').setView([-8.6571582470955, 115.22197239849751], 13);
+        const map = L.map('map').setView([{{$schedule->office->latitude}}, {{$schedule->office->longitude}}], 13);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+        const office = [{{$schedule->office->latitude}}, {{$schedule->office->longitude}}];
+        const radius = {{$schedule->office->radius}};
+        let marker;
+
+        const circle = L.circle(office, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: radius
+        }).addTo(map);
+
+        function tagLocation() {
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const lat = position.coords.latitude;
+                    const lng = position.coords.longitude;
+
+                    if(marker) {
+                        map.removeLayer(marker);
+                    }
+
+                    marker = L.marker([lat, lng]).addTo(map);
+                    map.setView([lat, lng], 13);
+
+                    if(isWithinRadius(lat, lng, office, radius)) {
+                        alert('Dalam radius');
+                    } else {
+                        alert('Tidak dalam radius');
+                    }
+                })
+            } else {
+                alert('Tidak bisa get location');
+            }
+        }
+
+        function isWithinRadius(lat, lng, center, radius) {
+            let distance = map.distance([lat, lng], center);
+            return distance <= radius;
+        }
     </script>
 </div>
